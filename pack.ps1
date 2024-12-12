7b68b035def84545baacd30ef6d94a427668aeb0
Enita Nureya <53754724+lingrottin@users.noreply.github.com>
2024-12-12 18:15:36 +0800

    add files

---
+++ pack.ps1
@@ -0,0 +1,2 @@
+New-Item -Name "dist" -ItemType Directory
+Compress-Archive -Path "templates", "theme.yaml", "settings.yaml", "README.md", "LICENSE.md" -DestinationPath "dist/halo-theme-mainroad-uof.zip"
